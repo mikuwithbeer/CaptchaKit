b8c9c2f4113cc3467f030c098efd24e6f8812e40
mikuwithbeer <git@miku.beer>
2025-06-30 19:09:18 +0300

    create package

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CaptchaKit",
+    platforms: [
+        .macOS(.v10_15)
+    ],
+    products: [
+        .library(
+            name: "CaptchaKit",
+            targets: ["CaptchaKit"])
+    ],
+    targets: [
+        .target(
+            name: "CaptchaKit"),
+        .testTarget(
+            name: "CaptchaKitTests",
+            dependencies: ["CaptchaKit"]
+        ),
+    ]
+)
